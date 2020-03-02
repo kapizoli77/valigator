f842d4ffd0df2f92c8675e79c8b06d17e468f9f5
Kapi Zoltán <z.kapi@mito.hu>
2020-03-02 17:43:19 +0100

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(name: "Valigator",
+                      platforms: [.iOS(.v11)],
+                      products: [.library(name: "Valigator",
+                                          targets: ["Valigator"])],
+                      targets: [.target(name: "Valigator",
+                                        path: "Source")],
+                      swiftLanguageVersions: [.v5])
